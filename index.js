da5c6b59b285aedfe8403f0aa556d68319e28d91
Rishav Mishra <47040257+erober@users.noreply.github.com>
2019-12-25 19:36:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const login= require('./auth/login')
 const user= require('./routes/user')
 const cont= require('./routes/cont')
 
-mongoose.connect("mongodb+srv://gabbar329:mongodb@cluster0-718ce.mongodb.net/user?retryWrites=true&w=majority");
+mongoose.connect("mongodb+srv://gabbar329:mango123@cluster0-718ce.mongodb.net/user?retryWrites=true&w=majority");
 
 
 const app = express();
